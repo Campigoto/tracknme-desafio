e4272543786dc50bba2f8293323e4a35097dfdf6
Evandro Campigoto <campigoto@hotmail.com>
2022-05-18 21:17:23 -0300

    Inicio do desafio

---
+++ backend/src/main/java/com/campigoto/crudfuncionarios/CrudfuncionariosApplication.java
@@ -0,0 +1,13 @@
+package com.campigoto.crudfuncionarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudfuncionariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudfuncionariosApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/campigoto/crudfuncionarios/CrudfuncionariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campigoto.crudfuncionarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudfuncionariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
